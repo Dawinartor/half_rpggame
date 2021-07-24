9fdbab5409cb24db1984f67fe96f5500a209cebd
Dawinartor <46395323+Dawinartor@users.noreply.github.com>
2021-07-24 12:47:10 +0200

    Add some assets and tested scale function

--- Code/baseGame.js
+++ Code/baseGame.js
@@ -5,8 +5,8 @@ Jeder Script beschreibt eine Szene des eigentlichen Spiels
 
 var config = {
     type: Phaser.AUTO, // Wie das Spiel gerenderd wird: Erst WebGL, sonst Canvas
-    width: 360,
-    heigt: 180,
+    width: 320,
+    height: 220, // um 40 px erhoet um Dialog hinzuzufuegen
     scene: { // Funktionen die wir weiter unten verwenden
         preload: preload,
         create: create,
@@ -21,16 +21,25 @@ var game = new Phaser.Game(config);
 function preload() 
 {
     // Dieses Sprite nennt sich nun "ground"
-    this.load.image('ground', '../Elemente/Assets/Mapbeispiel/TestMap.png'); 
+    this.load.image('ground', '../Elemente/Assets/Mapbeispiel/TestMap.png');
+    this.load.image('dialog', '../Elemente/Assets/Dialoge/baseDialog.png');
+    this.load.image('woman', '../Elemente/Assets/Figurbeispiel/woman.png');
 }
 
 // Geladene Materialien anzeigen lassen
 function create() 
 {
-    this.add.image(0, 0, 'ground').setOrigin(0,0);
+    // Game basics
+    this.add.image(0, -1, 'ground').setOrigin(0,0);
+    this.add.image(0, 160, 'dialog').setOrigin(0,0);
+
+    // manipulation of images
+    npc_Woman = this.add.image(100, 0, 'woman').setOrigin(0,0);
+    npc_Woman.setScale(0.1)
 }
 
 
-function update() {
+function update() 
+{
 
 }
